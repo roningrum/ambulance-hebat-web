4994b46debc3eefcff602992a89f98aa2e0d2f5e
Ota <rizkioktavianingrum@gmail.com>
2022-06-08 10:30:14 +0700

    edit navbar and scroll

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,7 @@ window.addEventListener('scroll', function(){
         navbar.classList.replace('nav-color','bg-transparent');
     }
 });
+
+$(document).ready(function(){
+    $('html, body').scrollTop(0);
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,16 @@
 <!-- hero section -->
 <section id="hero">
     <div class="container hero-tagline">
-      <div class="row flex-lg-row-reverse align-items-center g-4">
-        <div class="col-sm-8 col-lg-6">
-          <img src="imgs/ambulance.png" width="400px" height="400px" class="img-hero">
-        </div>
-        <div class="col-lg-6 col-sm-4">
+      <div class="row">
+        <div class="col-lg-6">
           <h1>Layanan Ambulance Andalan Masyarakat Kota Semarang</h1>
           <p>Ambulance Hebat siap melayani penanganan gawat darurat di Kota Semarang</p>
           <button class="button-lg-primary">
             <i class="fa-solid fa-phone"></i>
             Call 1500-132</button>
+        </div>
+        <div class="col-lg-6">
+          <img src="imgs/ambulance.png"class="img-fluid d-block">
         </div>
       </div>
     </div>
@@ -21,17 +21,17 @@
     <!-- section short description -->
     <section id="desc">
         <div class="container col-xxl-6 px-3 py-5">
-          <div class="row flex-lg-row align-items-center g-4 desc-info">
-            <div class=" col-lg-6 col-sm-6 col-md-8 ">
-              <img src="imgs/foto-aha1.png" width="100%" alt="">
+          <div class="row flex-lg-row align-items-center g-5 desc-info">
+            <div class=" col-lg-6">
+              <img src="imgs/foto-aha1.png" width="100%" class="img-fluid"alt="">
             </div>
-            <div class=" col-lg-6 col-md-8 col-sm-6 px-5">
+            <div class=" col-lg-6">
               <h1>Ambulance Hebat SiCepat</h1>
               <p>Layanan emergenci untuk penangan kasus kegawatdaruratan di Kota Semarang. Ambulan hebat adalah salah satu
                 layanan gratis yang disediakan oleh Pemerintah Kota Semarang, dibawah asuhan Dinas Kesehatan Kota Semarang
               </p>
               <button class="button-more">
-                <a href="#"></a>
+                <a href="/about"></a>
                 Selengkapnya</button>
             </div>
           </div>
@@ -41,44 +41,44 @@
         <!--service-->
   <section id="service">
     <div class="container">
-      <div class="row">
+      <div class="row g-3">
         <div class="col-12 text-center service-headline">
           <h2>Keunggulan Ambulance Hebat</h2>
           <p>Ambulance Hebat siap melayani karena</p>
         </div>
       </div>
-      <div class="row d-flex align-items-center text-center mt-3">
-        <div class="col-lg-4 col-sm-6 p-2">
+      <div class="row align-items-center text-center mt-3">
+        <div class="col-lg-4 p-2">
           <div class="card-layanan">
             <div class="circle-icon position-relative mx-auto mt-5">
               <img src="imgs/24jam.png" class="position-absolute top-50 start-50 translate-middle" alt=""
                 srcset="">
             </div>
-            <h3>Siaga 24 Jam</h3>
+            <h3 class="mt-4">Siaga 24 Jam</h3>
             <p>Ambulan Hebat siap melayani
               gawat darurat 24 jam</p>
           </div>
         </div>
-        <div class="col-lg-4 col-sm-6 p-2">
+        <div class="col-lg-4 p-2">
           <div class="card-layanan">
             <div class="circle-icon position-relative mx-auto mt-5">
               <img src="imgs/profesional.png" class="position-absolute top-50 start-50 translate-middle" alt=""
                 srcset="">
             </div>
-            <h3 class="mt-2">Tenaga Profesional</h3>
+            <h3 class="mt-4">Tenaga Profesional</h3>
             <p>Ditangani langsung oleh dokter,
               tenaga medis,
               perawat dan driver terlatih</p>
           </div>
 
         </div>
-        <div class="col-lg-4 col-sm-6 p-2 text-center">
+        <div class="col-lg-4 p-2 text-center">
           <div class="card-layanan">
             <div class="circle-icon position-relative mx-auto mt-5">
               <img src="imgs/free.png" class="position-absolute top-50 start-50 translate-middle" alt=""
                 srcset="">
             </div>
-            <h3 class="mt-2">Gratis</h3>
+            <h3 class="mt-4">Gratis</h3>
             <p>Layanan Ambulan Hebat
               tidak dipungut
               biaya</p>
